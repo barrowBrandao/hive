9051ca5fc2c219cb7562d4b9a95e53b03229c7f9
Carlos Brandao <121578173+barrowBrandao@users.noreply.github.com>
2023-05-21 18:46:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+function ValidateFields() {
+    btnDisable();
+    userErro()
+    
+  }
+  
+  function login() {
+    firebase.auth().signInWithEmailAndPassword(
+        form.usuario().value, form.senha().value
+    ).then(response => {
+        window.location.href = "main.html";
+    }).catch(error => {
+        alert(mensagemErro (error));
+    });
+    
+  }
+
+  function mensagemErro (error){
+    if (error.code == "auth/user-not-found"){
+        return "Usuário não encontrado";
+    }  
+    return error.message;
+
+  }
+
+  function userlValido (){
+    const user = form.usuario().value;
+    if (!user){
+        return false;
+    }
+    return validadeUser(user);
+
+  }
+  function userErro(){
+    const user = form.usuario().value;
+    form.emailObrigatorio().style.display = user ? "none" : "block";
+    form.emailInvalido().style.display = validadeUser(user) ? "none" : "block";
+  }
+
+  function passwordError () {
+    const senha = form.senha().value;
+    form.senhaInvalida().style.display = senha ? "none" : "block";
+  }
+
+  function btnDisable(){
+    const senha = isPasswordValid();
+    form.loginButton().disabled = !senha;
+  }
+  
+  function isPasswordValid () {
+    const senha = form.senha().value;
+    if(!senha) {
+      return false;
+    }
+    return true;
+  }
+
+
+  const form = {
+    usuario: () => document.getElementById('usuario'),
+    senha: () => document.getElementById('senha'),
+    emailInvalido: () => document.getElementById('user-error'),
+    emailObrigatorio: () => document.getElementById('user-erro'),
+    loginButton: () => document.getElementById('btn-login'),
+    senhaInvalida: () => document.getElementById('senha-erro')
+  }
---
+++ validations.js
@@ -0,0 +1,3 @@
+function validadeUser(user){
+    return user;
+  }s
